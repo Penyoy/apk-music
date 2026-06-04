b8667b3a21a5dd4cc21eb19b9dad3c77ca747f15
Penyoy <penyoy11@gmail.com>
2026-06-04 16:29:38 +0700

    Add material and material-icons-core dependencies

--- sonicplayer/app/build.gradle.kts
+++ sonicplayer/app/build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.animation:animation")
+    implementation("androidx.compose.material:material")
+implementation("androidx.compose.material:material-icons-core")
     
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.7")
